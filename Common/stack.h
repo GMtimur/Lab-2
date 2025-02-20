77c9beb6ea62e463dba609b41034e4c770c2f3e1
GMtimur <gmtimurtwitch@gmail.com>
2025-02-20 21:40:30 +0300

    Lab1 Error fix (3)

--- Common/stack.h
+++ Common/stack.h
@@ -3,21 +3,12 @@
 
 using namespace std;
 
-template <typename T>
-
 /// <summary>
 /// Класс `Stack` реализует структуру данных "стек" с использованием связного списка.
 /// Стек работает по принципу "Последний пришел, первый вышел" (LIFO — Last In, First Out).
 /// </summary>
 /// <typeparam name="T">Тип данных, хранящихся в стеке.</typeparam>
-/// <remarks>
-/// Основные операции:
-/// - `push(T value)`: Добавляет элемент на вершину стека.
-/// - `pop()`: Удаляет и возвращает элемент с вершины стека.
-/// - `peek()`: Возвращает элемент с вершины стека без его удаления.
-/// - `count()`: Возвращает количество элементов в стеке.
-/// - `clear()`: Очищает стек.
-/// </remarks>
+template <typename T>
 class Stack 
 {
 private:
@@ -37,24 +28,40 @@ public:
         clear();
     }
 
+    /// <summary>
+    /// Добавляет элемент на вершину стека.
+    /// </summary>
+    /// <param name="value">Значение, которое нужно добавить в стек.</param>
     void push(T value) 
-	{
+    {
         top = new Node(value, top); 
         counter++;
     }
 
+    /// <summary>
+    /// Удаляет и возвращает элемент с вершины стека.
+    /// </summary>
+    /// <returns>Значение элемента на вершине стека.</returns>
+    /// <exception cref="out_of_range">Выбрасывается, если стек пуст.</exception>
     T pop() 
     {
         if (top == nullptr) 
         {
             throw out_of_range("Stack is empty");
         }
-        T value = top->data;
-	    top = top->next;
+        Node* temp = top;
+        T value = temp->data;
+        top = top->next;
+        delete temp;
         counter--;
         return value;
     }
 
+    /// <summary>
+    /// Возвращает значение элемента на вершине стека без его удаления.
+    /// </summary>
+    /// <returns>Значение элемента на вершине стека.</returns>
+    /// <exception cref="out_of_range">Выбрасывается, если стек пуст.</exception>
     T peek() const 
     {
         if (top == nullptr) 
@@ -64,11 +71,18 @@ public:
         return top -> data;
     }
 
+    /// <summary>
+    /// Возвращает количество элементов в стеке.
+    /// </summary>
+    /// <returns>Количество элементов в стеке.</returns>
     int count() const 
     {
-        return size;
+        return counter;
     }
 
+    /// <summary>
+    /// Очищает стек, удаляя все элементы.
+    /// </summary>
     void clear() 
     {
         while (top != nullptr) 
--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -9,18 +9,18 @@ int main()
     int value;
     
     while(cin >> value && value != 0) 
-	{
+    {
         stack.push(value);
     }
     
     while(stack.count() > 0) 
-	{
+    {
         int num = stack.pop();
         if(num > 0) 
-		{
+        {
             cout << num << " ";
         }
     }
-    
+
     return 0;
 }
