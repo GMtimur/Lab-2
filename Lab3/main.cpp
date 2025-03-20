7dff9654515b9c2b2b0b1e908100b2e7c025bb77
GMtimur <gmtimurtwitch@gmail.com>
2025-03-20 20:07:23 +0300

    Lab3 fix

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -28,7 +28,8 @@ void insertBeforeNegatives(CycleList<int>& list)
 void removeNegatives(CycleList<int>& list)
 {
     int i = 0;
-    while (i < list.count()){
+    while (i < list.count())
+    {
         if (list[i] < 0)
         {
             list.removeAt(i);
@@ -55,18 +56,93 @@ void printList(CycleList<int>& list)
 
 void displayMenu()
 {
-    cout << "1. Добавить элемент\n";
-    cout << "2. Вставить элемент\n";
-    cout << "3. Удалить элемент по индексу\n";
-    cout << "4. Получить элемент по индексу\n";
-    cout << "5. Посчитать количество элементов\n";
-    cout << "6. Посчитать количество вхождений значения\n";
-    cout << "7. Очистить список\n";
-    cout << "8. Вставить 1 перед каждым отрицательным элементом\n";
-    cout << "9. Удалить все отрицательные элементы\n";
-    cout << "10. Вывести список\n";
-    cout << "0. Выйти\n";
+    cout << "1. Добавить элемент" << endl;
+    cout << "2. Вставить элемент" << endl;
+    cout << "3. Удалить элемент по индексу" << endl;
+    cout << "4. Получить элемент по индексу" << endl;
+    cout << "5. Посчитать количество элементов" << endl;
+    cout << "6. Посчитать количество вхождений значения" << endl;
+    cout << "7. Очистить список" << endl;
+    cout << "8. Вставить 1 перед каждым отрицательным элементом" << endl;
+    cout << "9. Удалить все отрицательные элементы" << endl;
+    cout << "10. Вывести список" << endl;
+    cout << "0. Выйти" << endl;
     cout << "Выберите опцию: ";
+}
+
+int cases(int choice, int value, int index, CycleList<int> list)
+{
+    switch (choice)
+    {
+        case 1:
+            cout << "Введите значение: ";
+            cin >> value;
+            list.add(value);
+            break;
+        case 2:
+            cout << "Введите индекс и значение: ";
+            cin >> index >> value;
+            list.insert(index, value);
+            break;
+        case 3:
+            cout << "Введите индекс: ";
+            cin >> index;
+            list.removeAt(index);
+            break;
+        case 4:
+            cout << "Введите индекс: ";
+            cin >> index;
+            try
+            {
+                cout << "Элемент с индексом " << index << ": " << list[index] << endl;
+            }
+            catch (out_of_range& e)
+            {
+                cout << e.what() << endl;
+            }
+            break;
+        case 5:
+            cout << "Количество элементов: " << list.count() << endl;
+            break;
+        case 6:
+            cout << "Введите значение: ";
+            cin >> value;
+            cout << "Количество вхождений " << value << ": " << list.countOccurrences(value) << endl;
+            break;
+        case 7:
+            list.clear();
+            cout << "Список очищен." << endl;
+            break;
+        case 8:
+            if(list.count() == 0)
+            {
+                cout << "Список пуст" << endl;
+            }
+            else
+            {
+                insertBeforeNegatives(list);
+                cout << "Вставлены 1 перед отрицательными элементами." << endl;
+            }
+            break;
+        case 9:
+            if(list.count() == 0)
+            {
+                cout << "Список пуст" << endl;
+            }
+            else
+            {
+                removeNegatives(list);
+                cout << "Отрицательные элементы удалены." << endl;
+            }
+            break;
+        case 10:
+            printList(list);
+            break;
+        case 0:
+            return 0;
+        default:
+            cout << "Неверная опция." << endl;
+    }
 }
 
 int main()
@@ -84,77 +160,7 @@ int main()
             cout << "Ошибка: введите число от 0 до 10." << endl;
             displayMenu();
         }
-        switch (choice)
-        {
-            case 1:
-                cout << "Введите значение: ";
-                cin >> value;
-                list.add(value);
-                break;
-            case 2:
-                cout << "Введите индекс и значение: ";
-                cin >> index >> value;
-                list.insert(index, value);
-                break;
-            case 3:
-                cout << "Введите индекс: ";
-                cin >> index;
-                list.removeAt(index);
-                break;
-            case 4:
-                cout << "Введите индекс: ";
-                cin >> index;
-                try
-                {
-                    cout << "Элемент с индексом " << index << ": " << list[index] << endl;
-                }
-                catch (out_of_range& e)
-                {
-                    cout << e.what() << endl;
-                }
-                break;
-            case 5:
-                cout << "Количество элементов: " << list.count() << endl;
-                break;
-            case 6:
-                cout << "Введите значение: ";
-                cin >> value;
-                cout << "Количество вхождений " << value << ": " << list.countOccurrences(value) << endl;
-                break;
-            case 7:
-                list.clear();
-                cout << "Список очищен." << endl;
-                break;
-            case 8:
-                if(list.count() == 0)
-                {
-                    cout << "Список пуст" << endl;
-                }
-                else
-                {
-                    insertBeforeNegatives(list);
-                    cout << "Вставлены 1 перед отрицательными элементами." << endl;
-                }
-                break;
-            case 9:
-                if(list.count() == 0)
-                {
-                    cout << "Список пуст" << endl;
-                }
-                else
-                {
-                    removeNegatives(list);
-                    cout << "Отрицательные элементы удалены." << endl;
-                }
-                break;
-            case 10:
-                printList(list);
-                break;
-            case 0:
-                return 0;
-            default:
-                cout << "Неверная опция." << endl;
-        }
+        cases(choice, value, index, list);
     }
 
     return 0;
